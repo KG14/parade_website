6b3fc0ed8799d2f115b9132d82b02a07fdb689e1
Karan Gupta <tokarangupta@gmail.com>
2023-12-13 11:46:06 -0800

    added livestream and snowflake effects

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $("body").flurry({
+          character: "❄",
+          color: "white",
+          height: 400,
+          frequency: 80,
+          speed: 4000,
+          small: 8,
+          large: 48,
+          wind: 60,
+          windVariance: 20,
+          rotation: 0,
+          rotationVariance: 90,
+        });
+      });
+});
