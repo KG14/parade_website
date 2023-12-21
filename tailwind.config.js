4b6ac485dbcc92f8b50aad203a6a85676d86770b
Karan Gupta <tokarangupta@gmail.com>
2023-12-20 18:48:03 -0800

    added link to amador in about page plus in navbar logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./templates/*.{html,js}"],
     theme: {
       extend: {},
+      /*fontFamily: {
+        'sans': ['Manrope', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+      }*/
     },
     plugins: [],
   }
